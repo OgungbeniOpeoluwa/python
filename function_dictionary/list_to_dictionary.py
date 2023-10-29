ed28fd465387b4cc0d8e6b20463e624deccca774
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-10-29 10:36:20 +0100

    update

--- function_dictionary/list_to_dictionary.py
+++ function_dictionary/list_to_dictionary.py
@@ -1,12 +1,65 @@
 def function_list_to_dictionary(result: list):
     dictionary = {}
-    actual = 0
     for count in result:
-        dictionary[count[0]] = count
-        if count[0] == dictionary.keys() and dictionary.keys() != dictionary.values():
+        if count[0] in dictionary.keys():
             result = str(count[0]).upper()
             dictionary[result] = count
+        else:
+            dictionary[count[0]] = count
     return dictionary
 
 
-print(function_list_to_dictionary(['apple', 'banana', 'coconut','corn']))
+def difference_in_list(result):
+    product = max(result) - min(result)
+    return product
+
+
+def two_list_to_dictionary(result1: list, result2: list):
+    result = {}
+    for count in range(len(result1)):
+        result[result1[count]] = result2[count]
+
+    return result
+
+
+def remove_duplicate_numbers_function(result: list, number):
+    counter = 0
+    count = 0
+    results = []
+    while counter < len(result):
+        for counts in range(len(result)):
+            if result[counter] == result[counts]:
+                count += 1
+        if count > number:
+            results.append(result[counter])
+        counter += count
+        count = 0
+    return results
+
+
+def remove_multiple_string_function(result):
+    product = []
+    counter = ""
+    for count in result:
+        if count.isalpha():
+            product.append(count)
+        else:
+            for counts in count:
+                if counts.isalpha():
+                    counter += counts
+                else:
+                    if counter.isalpha():
+                        product.append(counter)
+                    counter = ""
+    return product
+
+
+def split_list_function(result: list):
+    results = []
+    returns = []
+    product = len(result) // 2
+    for count in range(product):
+        results.append(result[count])
+    for counts in range(product, len(result)):
+        returns.append(result[counts])
+    return results, returns
--- test/function_dictionary/list_to_dictionary_test.py
+++ test/function_dictionary/list_to_dictionary_test.py
@@ -14,6 +14,41 @@ class MyTestCase(unittest.TestCase):
         expected = {'a': 'apple', 'b': 'banana', 'c': 'coconut', 'C': 'corn'}
         self.assertEqual(expected, list_to_dictionary.function_list_to_dictionary(result))
 
+    def test_taking_two_list_and_return_dictionary(self):
+        result1 = ['a', 'b', 'c', 'd', 'e']
+        result2 = [1, 2, 3, 4, 5]
+        result = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+        self.assertEqual(result, list_to_dictionary.two_list_to_dictionary(result1, result2))
+
+    def test_something(self):
+        result = [10, 75, 20, 30, 15, 5, 40, 25, 40, 35]
+        self.assertEqual(70, list_to_dictionary.difference_in_list(result))
+
+    def test_removing_multiple_string(self):
+        results = [",'ABC','xyz',", 'abc', 'XYZ']
+        expected = ['ABC', 'xyz', 'abc', 'XYZ']
+        self.assertEqual(expected, list_to_dictionary.remove_multiple_string_function(results))
+
+    def test_remove_duplicate_numbers_base_on_value_given(self):
+        result = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 5, 5, 5, 6, 7]
+        expect = [1, 2, 5]
+        self.assertEqual(expect, list_to_dictionary.remove_duplicate_numbers_function(result, 2))
+
+    def test_remove_duplicate_another_numbers_base_on_value_given(self):
+        result = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 5, 5, 5, 6, 7]
+        expect = [1, 2, 3, 5]
+        self.assertEqual(expect, list_to_dictionary.remove_duplicate_numbers_function(result, 1))
+
+    def test_split_items(self):
+        result = [10, 75, 20, 30, 15, 5, 40, 25, 40, 35]
+        expected = [10, 75, 20, 30, 15], [5, 40, 25, 40, 35]
+        self.assertEqual(expected, list_to_dictionary.split_list_function(result))
+
+    def test_if_list_is_not_the_same_length(self):
+        result = [10, 75, 20, 30, 15, 5, 40, 25, 40, 35, 45]
+        expected = [10, 75, 20, 30, 15], [5, 40, 25, 40, 35, 45]
+        self.assertEqual(expected, list_to_dictionary.split_list_function(result))
+
 
 if __name__ == '__main__':
     unittest.main()
